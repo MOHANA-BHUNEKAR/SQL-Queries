a68d9ff4f7c9c22bf8168892bcbd9170528ab4e6
Mohana Krishna Bhunekar <mohana.krishna08@gmail.com>
2016-05-23 15:41:48 -0700

    Update second-highest-salary.sql

--- second-highest-salary.sql
+++ second-highest-salary.sql
@@ -1,14 +1,12 @@
-# Time:  O(n)
-# Space: O(1)
-#
-# Write a SQL query to get the second highest salary from the Employee table.
+
+# Write a SQL query to get the second highest salary from the Employee table with Id and Salary as Employee table schema.
 #
 # +----+--------+
 # | Id | Salary |
 # +----+--------+
-# | 1  | 100    |
-# | 2  | 200    |
-# | 3  | 300    |
+# | 1  | 1000   |
+# | 2  | 2000    |
+# | 3  | 3000   |
 # +----+--------+
 # For example, given the above Employee table, the second highest salary is 200. If there is no second highest salary, then the query should return null.
 #
